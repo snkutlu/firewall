c72e8686a90380ec9ebc219264929d537398589c
Suleyman Kutlu <skutlu@schubergphilis.com>
2016-08-22 12:03:33 +0200

    Changing rule hadling process for Windows Firewall

--- libraries/helpers_windows.rb
+++ libraries/helpers_windows.rb
@@ -34,10 +34,6 @@ module FirewallCookbook
         shell_out!('netsh advfirewall reset')
       end
 
-      def add_rule!(params)
-        shell_out!("netsh advfirewall #{params}")
-      end
-
       def delete_all_rules!
         shell_out!('netsh advfirewall firewall delete rule name=all')
       end
@@ -46,41 +42,114 @@ module FirewallCookbook
         cmd = new_resource.command
         type = if cmd == :reject || cmd == :deny
                  :block
+               elsif cmd == :log
+                 :log
                else
                  :allow
                end
         type
       end
 
-      def build_rule(new_resource)
-        type = to_type(new_resource)
-        parameters = {}
-
-        parameters['description'] = "\"#{new_resource.description}\""
-        parameters['dir'] = new_resource.direction
-
-        new_resource.program && parameters['program'] = new_resource.program
-        parameters['service'] = new_resource.service ? new_resource.service : 'any'
-        parameters['protocol'] = new_resource.protocol
-
-        if new_resource.direction.to_sym == :out
-          parameters['localip'] = new_resource.source ? fixup_cidr(new_resource.source) : 'any'
-          parameters['localport'] = new_resource.source_port ? port_to_s(new_resource.source_port) : 'any'
-          parameters['interfacetype'] = new_resource.interface ? new_resource.interface : 'any'
-          parameters['remoteip'] = new_resource.destination ? fixup_cidr(new_resource.destination) : 'any'
-          parameters['remoteport'] = new_resource.dest_port ? port_to_s(new_resource.dest_port) : 'any'
-        else
-          parameters['localip'] = new_resource.destination ? new_resource.destination : 'any'
-          parameters['localport'] = dport_calc(new_resource) ? port_to_s(dport_calc(new_resource)) : 'any'
-          parameters['interfacetype'] = new_resource.dest_interface ? new_resource.dest_interface : 'any'
-          parameters['remoteip'] = new_resource.source ? fixup_cidr(new_resource.source) : 'any'
-          parameters['remoteport'] = new_resource.source_port ? port_to_s(new_resource.source_port) : 'any'
+      def build_rule(name, parameters)
+        # Build firewall command to execute the rule
+        rule = ''
+        unless parameters['firewall_command'] == 'do_nothing'
+          partial_command = parameters.sort.map { |k, v| "#{k}=#{v}" unless (v == '' || k == 'firewall_command') }.join(' ')
+          partial_command = "new #{partial_command}" if parameters['firewall_command'] == 'set'
+          rule = "firewall #{parameters['firewall_command']} rule name=\"#{name}\" #{partial_command}"
         end
+        rule
+      end
 
-        parameters['action'] = type.to_s
+      def execute_rule(rule)
+        unless rule.empty?
+          Chef::Log.debug "Firewall debug: Executing command: netsh advfirewall #{rule}"
+          shell_out!("netsh advfirewall #{rule}")
+        end
+      end
 
-        partial_command = parameters.map { |k, v| "#{k}=#{v}" }.join(' ')
-        "firewall add rule name=\"#{new_resource.name}\" #{partial_command}"
+      def execute_rules(rule_hash)
+        rule_hash.each do |name, parameters|
+          execute_rule(build_rule(name, parameters))
+        end
+      end
+
+      def parse_desired_rule_parameters(new_resource)
+        parameters = {}
+        unless to_type(new_resource) == :log
+          parameters['description'] = "\"#{new_resource.description}\""
+          parameters['dir'] = new_resource.direction
+
+          new_resource.program && parameters['program'] = "\"#{new_resource.program}\""
+          new_resource.service && parameters['service'] = new_resource.service
+          parameters['protocol'] = if new_resource.protocol == :icmp
+                                     :icmpv4
+                                   else
+                                     new_resource.protocol
+                                   end
+
+          if parameters['protocol'].to_s.match(/icmp/)
+            if new_resource.icmp_type
+              parameters['protocol'] = "#{parameters['protocol']}:#{new_resource.icmp_type}"
+              parameters['protocol'] << ",#{new_resource.icmp_code}" if new_resource.icmp_code
+            end
+          end
+
+          if new_resource.direction.to_sym == :out
+            parameters['localip'] = new_resource.source ? fixup_cidr(new_resource.source) : 'any'
+            parameters['localport'] = if parameters['protocol'].match(/icmp/)
+                                        ''
+                                      else
+                                        new_resource.source_port ? port_to_s(new_resource.source_port) : 'any'
+                                      end
+            parameters['interfacetype'] = new_resource.interface ? new_resource.interface : 'any'
+            parameters['remoteip'] = new_resource.destination ? fixup_cidr(new_resource.destination) : 'any'
+            parameters['remoteport'] = if parameters['protocol'].match(/icmp/)
+                                         ''
+                                       else
+                                         new_resource.dest_port ? port_to_s(new_resource.dest_port) : 'any'
+                                       end
+          else
+            parameters['localip'] = new_resource.destination ? new_resource.destination : 'any'
+            parameters['localport'] = if parameters['protocol'].match(/icmp/)
+                                        ''
+                                      else
+                                        dport_calc(new_resource) ? port_to_s(dport_calc(new_resource)) : 'any'
+                                      end
+            parameters['interfacetype'] = new_resource.dest_interface ? new_resource.dest_interface : 'any'
+            parameters['remoteip'] = new_resource.source ? fixup_cidr(new_resource.source) : 'any'
+            parameters['remoteport'] = if parameters['protocol'].match(/icmp/)
+                                         ''
+                                       else
+                                         new_resource.source_port ? port_to_s(new_resource.source_port) : 'any'
+                                       end
+          end
+
+          parameters['action'] = to_type(new_resource).to_s
+        end
+        parameters
+      end
+
+      def build_firewall_rule_hash(firewall_rule, desired_rules, current_rules)
+        type = to_type(firewall_rule)
+        rule_name = type == :log ? firewall_rule.logging.to_s : firewall_rule.name
+        desired_rules[type.to_s] ||= {}
+
+        # If it is a logging rule handle properly
+        if type == :log
+          desired_rules[type.to_s][rule_name]['action'] = 'enable'
+        elsif desired_rules[type.to_s].key?(rule_name)
+          Chef::Log.warn "Firewall rule named #{rule_name} already exists"
+        else
+          desired_rules[type.to_s][rule_name] = parse_desired_rule_parameters(firewall_rule)
+          desired_rules[type.to_s][rule_name]['firewall_command'] = if !current_rules.key?(rule_name)
+                                                                      'add'
+                                                                    elsif rule_up_to_date?(desired_rules[type.to_s][rule_name], current_rules[rule_name])
+                                                                      'do_nothing'
+                                                                    else
+                                                                      'set'
+                                                                    end
+        end
       end
 
       def rule_exists?(name)
@@ -90,6 +159,32 @@ module FirewallCookbook
         end
       end
 
+      def retrieve_current_profile
+        shell_out!('netsh advfirewall show currentprofile').stdout.match(/(\w+)\s+Profile Settings:/)[1]
+      end
+
+      def retrieve_current_logging
+        logging = {}
+        cmd = shell_out!('netsh advfirewall show currentprofile logging')
+        cmd.stdout.chomp.split(/\r\n/).each do |line|
+          logging['allowedconnections'] = Regexp.last_match(1).downcase if line =~ /^LogAllowedConnections\s+(\w+)$/
+          logging['droppedconnections'] = Regexp.last_match(1).downcase if line =~ /^LogDroppedConnections\s+(\w+)$/
+        end
+        logging
+      end
+
+      def retrieve_current_policy
+        policy = {}
+        cmd = shell_out!('netsh advfirewall show currentprofile firewallpolicy')
+        cmd.stdout.chomp.split(/\r\n/).each do |line|
+          if line =~ /^Firewall Policy\s+(\w+),(\w+)$/
+            policy['inbound'] = Regexp.last_match(1).downcase
+            policy['outbound'] = Regexp.last_match(2).downcase
+          end
+        end
+        policy
+      end
+
       def show_all_rules!
         cmd = shell_out!('netsh advfirewall firewall show rule name=all')
         cmd.stdout.each_line do |line|
@@ -97,33 +192,56 @@ module FirewallCookbook
         end
       end
 
-      def rule_up_to_date?(name, type)
-        @up_to_date ||= begin
-          desired_parameters = rule_parameters(type)
-          current_parameters = {}
-
-          cmd = shell_out!("netsh advfirewall firewall show rule name=\"#{name}\" verbose")
-          cmd.stdout.each_line do |line|
-            current_parameters['description'] = "\"#{Regexp.last_match(1).chomp}\"" if line =~ /^Description:\s+(.*)$/
-            current_parameters['dir'] = Regexp.last_match(1).chomp if line =~ /^Direction:\s+(.*)$/
-            current_parameters['program'] = Regexp.last_match(1).chomp if line =~ /^Program:\s+(.*)$/
-            current_parameters['service'] = Regexp.last_match(1).chomp if line =~ /^Service:\s+(.*)$/
-            current_parameters['protocol'] = Regexp.last_match(1).chomp if line =~ /^Protocol:\s+(.*)$/
-            current_parameters['localip'] = Regexp.last_match(1).chomp if line =~ /^LocalIP:\s+(.*)$/
-            current_parameters['localport'] = Regexp.last_match(1).chomp if line =~ /^LocalPort:\s+(.*)$/
-            current_parameters['interfacetype'] = Regexp.last_match(1).chomp if line =~ /^InterfaceTypes:\s+(.*)$/
-            current_parameters['remoteip'] = Regexp.last_match(1).chomp if line =~ /^RemoteIP:\s+(.*)$/
-            current_parameters['remoteport'] = Regexp.last_match(1).chomp if line =~ /^RemotePort:\s+(.*)$/
-            current_parameters['action'] = Regexp.last_match(1).chomp if line =~ /^Action:\s+(.*)$/
+      def parse_current_rule_parameters(rule_list)
+        name = ''
+        all_rules = {}
+        rule_list.each_with_index do |line, i|
+          # Bypass empty lines
+          next if line.length.zero?
+          # First find the rule name
+          if line =~ /^Rule Name:\s+(.*)$/
+            name = Regexp.last_match(1).chomp
+            all_rules[name] ||= {}
           end
-
-          up_to_date = true
-          desired_parameters.each do |k, v|
-            up_to_date = false if current_parameters[k] !~ /^["]?#{v}["]?$/i
+          # We now have name then fill the hash for that named rule
+          all_rules[name]['description'] = "\"#{Regexp.last_match(1).chomp}\"" if line =~ /^Description:\s+(.*)$/
+          all_rules[name]['dir'] = Regexp.last_match(1).chomp if line =~ /^Direction:\s+(.*)$/
+          all_rules[name]['localip'] = Regexp.last_match(1).chomp if line =~ /^LocalIP:\s+(.*)$/
+          all_rules[name]['remoteip'] = Regexp.last_match(1).chomp if line =~ /^RemoteIP:\s+(.*)$/
+          # Handle protocol carefully
+          if line =~ /^Protocol:\s+(.*)$/
+            all_rules[name]['protocol'] = Regexp.last_match(1).chomp
+            # ICMP needs speclal handling
+            if all_rules[name]['protocol'].downcase =~ /icmp/
+              all_rules[name]['protocol'] += ":#{rule_list[i + 2].chomp.split(' ')[0]},#{rule_list[i + 2].chomp.split(' ')[1]}"
+            end
           end
-
-          up_to_date
+          all_rules[name]['localport'] = Regexp.last_match(1).chomp if line =~ /^LocalPort:\s+(.*)$/
+          all_rules[name]['remoteport'] = Regexp.last_match(1).chomp if line =~ /^RemotePort:\s+(.*)$/
+          all_rules[name]['program'] = Regexp.last_match(1).chomp if line =~ /^Program:\s+(.*)$/
+          all_rules[name]['service'] = Regexp.last_match(1).chomp if line =~ /^Service:\s+(.*)$/
+          all_rules[name]['interfacetype'] = Regexp.last_match(1).chomp if line =~ /^InterfaceTypes:\s+(.*)$/
+          all_rules[name]['action'] = Regexp.last_match(1).chomp if line =~ /^Action:\s+(.*)$/
         end
+        all_rules
+      end
+
+      def retrieve_all_rules
+        output = shell_out!('netsh advfirewall firewall show rule name=all verbose', returns: [0, 1])
+        if output.exitstatus.nonzero? && output.stdout =~ /^No rules match/
+          Chef::Log.warn('"No firewall rules found')
+          {}
+        else
+          parse_current_rule_parameters(output.stdout.sub(/^\r\n/, '').split(/\r\n/))
+        end
+      end
+
+      def rule_up_to_date?(desired_rule_parameters, current_rule_parameters)
+        up_to_date = true
+        desired_rule_parameters.each do |k, v|
+          up_to_date = false if current_rule_parameters[k].to_s.downcase !~ /^["]?#{v.to_s.downcase}["]?$/i
+        end
+        up_to_date
       end
     end
   end
--- libraries/provider_firewall_windows.rb
+++ libraries/provider_firewall_windows.rb
@@ -1,5 +1,6 @@
 #
 # Author:: Sander van Harmelen (<svanharmelen@schubergphilis.com>)
+# Updated:: Suleyman Kutlu (<skutlu@schubergphilis.com>)
 # Cookbook Name:: firewall
 # Provider:: windows
 #
@@ -39,47 +40,61 @@ class Chef
     action :restart do
       next if disabled?(new_resource)
 
-      # ensure it's initialized
-      new_resource.rules({}) unless new_resource.rules
-      new_resource.rules['windows'] = {} unless new_resource.rules['windows']
+      converge_by('Apply rule changes (if any) and restart Windows Firewall service') do
+        # ensure it's initialized
+        new_resource.rules({}) unless new_resource.rules
+        new_resource.rules['windows'] = {} unless new_resource.rules['windows']
+        new_resource.rules['windows']['log'] = {}
+        #
+        # By default logging is disabled on Windows Firewall. If no logging rules provided, these will be in effect.
+        #
+        new_resource.rules['windows']['log']['droppedconnections'] = {}
+        new_resource.rules['windows']['log']['droppedconnections']['action'] = 'disable'
+        new_resource.rules['windows']['log']['allowedconnections'] = {}
+        new_resource.rules['windows']['log']['allowedconnections']['action'] = 'disable'
 
-      firewall_rules = run_context.resource_collection.select { |item| item.is_a?(Chef::Resource::FirewallRule) }
-      firewall_rules.each do |firewall_rule|
-        next unless firewall_rule.action.include?(:create) && !firewall_rule.should_skip?(:create)
+        # Retrieve current running rules into a hash
+        current_rules = retrieve_all_rules
 
-        # build rules to apply with weight
-        k = build_rule(firewall_rule)
-        v = firewall_rule.position
-
-        # unless we're adding them for the first time.... bail out.
-        unless new_resource.rules['windows'].key?(k) && new_resource.rules['windows'][k] == v
-          new_resource.rules['windows'][k] = v
+        firewall_rules = run_context.resource_collection.select { |item| item.is_a?(Chef::Resource::FirewallRule) }
+        firewall_rules.each do |firewall_rule|
+          next unless firewall_rule.action.include?(:create) && !firewall_rule.should_skip?(:create)
+          build_firewall_rule_hash(firewall_rule, new_resource.rules['windows'], current_rules)
         end
-      end
 
-      # ensure a file resource exists with the current rules
-      begin
-        windows_file = run_context.resource_collection.find(file: windows_rules_filename)
-      rescue
-        windows_file = file windows_rules_filename do
-          action :nothing
+        # Apply rules in order:
+        # First logging rules
+        # -------------------
+        # If logging rules are not provided, then execute them
+        # execute_rules(new_resource.rules['windows']['log'])
+        current_logging = retrieve_current_logging
+        new_resource.rules['windows']['log'].each do |name, parameters|
+          execute_rule("set currentprofile logging #{name} #{parameters['action']}") unless current_logging[name] == parameters['action']
         end
-      end
-      windows_file.content build_rule_file(new_resource.rules['windows'])
-      windows_file.run_action(:create)
 
-      # if the file was changed, restart iptables
-      if windows_file.updated_by_last_action?
-        disable! if active?
-        delete_all_rules! # clear entirely
-        reset! # populate default rules
+        # Then apply block and allow rules in order
+        execute_rules(new_resource.rules['windows']['block']) unless new_resource.rules['windows']['block'].empty?
+        execute_rules(new_resource.rules['windows']['allow']) unless new_resource.rules['windows']['allow'].empty?
 
-        new_resource.rules['windows'].sort_by { |_k, v| v }.map { |k, _v| k }.each do |cmd|
-          add_rule!(cmd)
+        # If it is not desired to keep existing unwanted rules...
+        # Find the rules to be removed from current list and delete them
+        unless new_resource.keep_existing_rules
+          to_delete = {}
+          current_rules.each do |name, _parameters|
+            next if new_resource.rules['windows']['allow'].key?(name) || new_resource.rules['windows']['block'].key?(name)
+            to_delete[name] ||= {}
+            to_delete[name]['firewall_command'] = 'delete'
+          end
+          execute_rules(to_delete) unless to_delete.empty?
         end
-        # ensure it's enabled _after_ rules are inputted, to catch malformed rules
-        enable! unless active?
 
+        # Apply desired default firewall policy at the end
+        current_policy = retrieve_current_policy
+        desired_inbound_policy = node['firewall']['windows']['defaults']['policy']['inbound']
+        desired_outbound_policy = node['firewall']['windows']['defaults']['policy']['outbound']
+        unless current_policy['inbound'] == desired_inbound_policy && current_policy['outbound'] == desired_outbound_policy
+          execute_rule("set currentprofile firewallpolicy #{desired_inbound_policy},#{desired_outbound_policy}")
+        end
         new_resource.updated_by_last_action(true)
       end
     end
